2dad29cafcd5f48cc30de85a4d462852b9616966
Aditya Ramandhito <ramandhitoa@gmail.com>
2026-09-12 15:29:40 +0800

    Fix iOS PWA metadata

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      includeAssets: ['android-chrome-192x192.png', 'android-chrome-512x512.png'],
       manifest: {
         name: 'MineTrack Pro',
         short_name: 'MineTrack',
@@ -19,12 +19,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
+            src: '/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
